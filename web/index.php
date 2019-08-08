14e7fec494f5bb1cc56971a6a6c174d20b821f1c
Brad Bell <brad@pixelandtonic.com>
2019-08-08 10:29:10 -0700

    Update to DotEnv ^3.4

--- web/index.php
+++ web/index.php
@@ -12,7 +12,7 @@ require_once CRAFT_VENDOR_PATH.'/autoload.php';
 
 // Load dotenv?
 if (class_exists('Dotenv\Dotenv') && file_exists(CRAFT_BASE_PATH.'/.env')) {
-    (new Dotenv\Dotenv(CRAFT_BASE_PATH))->load();
+    DotEnv\DotEnv::create(CRAFT_BASE_PATH)->load();
 }
 
 // Load and run Craft
