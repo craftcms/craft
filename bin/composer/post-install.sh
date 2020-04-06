5b58f312cb1fe4d70e7d59c377169ba094a4ac87
Timmy O'Mahony <hey@timmyomahony.com>
2020-04-06 14:51:12 +0100

    Added script to create database

--- bin/composer/post-install.sh
+++ bin/composer/post-install.sh
@@ -3,5 +3,9 @@
 name=${PWD##*/} 
 slug="${name//-/_}"
 
+psql -c "create database $slug;"
+psql -c "create user $slug with password '$slug';"
+psql -c "grant all privileges on database $slug to $slug;"
+
 find . -type f ! -path '*/vendor/*' ! -path '*/bin/composer/*' -print0 | xargs -0 sed -i '' -e "s/\[\[name\]\]/$name/g"
 find . -type f ! -path '*/vendor/*' ! -path '*/bin/composer/*' -print0 | xargs -0 sed -i '' -e "s/\[\[slug\]\]/$slug/g"
