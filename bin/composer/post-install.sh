5b3f7946bd03cb0cd28a2712ed39ac5067aba114
Timmy O'Mahony <hey@timmyomahony.com>
2020-04-06 15:07:57 +0100

    Updated post install script

--- bin/composer/post-install.sh
+++ bin/composer/post-install.sh
@@ -2,10 +2,16 @@
 
 name=${PWD##*/} 
 slug="${name//-/_}"
+secret=$(head /dev/urandom | LC_ALL=C tr -dc A-Za-z0-9 | head -c 30 ; echo '')
 
-psql -c "create database $slug;"
-psql -c "create user $slug with password '$slug';"
-psql -c "grant all privileges on database $slug to $slug;"
+if [ "$( psql -tAc "SELECT 1 FROM pg_database WHERE datname='$slug'" )" != '1' ]
+then
+    psql -c "create database $slug;"
+    psql -c "create user $slug with password '$slug';"
+    psql -c "grant all privileges on database $slug to $slug;"
+fi
 
+# Replace all instances of [[name]], [[slug]] and [[secret]] in the project
 find . -type f ! -path '*/vendor/*' ! -path '*/bin/composer/*' -print0 | xargs -0 sed -i '' -e "s/\[\[name\]\]/$name/g"
 find . -type f ! -path '*/vendor/*' ! -path '*/bin/composer/*' -print0 | xargs -0 sed -i '' -e "s/\[\[slug\]\]/$slug/g"
+find . -type f ! -path '*/vendor/*' ! -path '*/bin/composer/*' -print0 | xargs -0 sed -i '' -e "s/\[\[secret\]\]/$secret/g"
