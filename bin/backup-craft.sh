a99938d15d80c1ad2bdedec36a8e4e5f7125f611
Timmy O'Mahony <hey@timmyomahony.com>
2020-05-01 11:09:12 +0100

    Purgecss, default plugin settings, mail settings, vscode updates

--- bin/backup-craft.sh
+++ bin/backup-craft.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# A simple script that uses remote-sync to backup
+# A simple script that uses craft-remote-backup
 /home/forge/[[name]].weareferal.com/craft remote-backup/database/create
 /home/forge/[[name]].weareferal.com/craft remote-backup/database/prune
 /home/forge/[[name]].weareferal.com/craft remote-backup/volume/create
