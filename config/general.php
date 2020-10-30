d9bc00b91534773a766aba042204881db9162915
Brandon Kelly <brandon@pixelandtonic.com>
2020-10-30 11:49:07 -0700

    capital S/P Staging/Production

--- config/general.php
+++ config/general.php
@@ -37,7 +37,7 @@ return [
 
     // Staging environment settings
     'staging' => [
-        // Set this to `false` to prevent administrative changes from being made on staging
+        // Set this to `false` to prevent administrative changes from being made on Staging
         'allowAdminChanges' => true,
 
         // Prevent crawlers from indexing pages and following links
@@ -46,7 +46,7 @@ return [
 
     // Production environment settings
     'production' => [
-        // Set this to `false` to prevent administrative changes from being made on production
+        // Set this to `false` to prevent administrative changes from being made on Production
         'allowAdminChanges' => true,
     ],
 ];
