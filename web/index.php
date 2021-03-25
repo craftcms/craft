e6bcaa47ea4924837e2bdb53a89a7cfa7026c9d5
brandonkelly <brandon@pixelandtonic.com>
2021-03-25 09:20:14 -0700

    Make it clearer where additional PHP constants should go

--- web/index.php
+++ web/index.php
@@ -15,8 +15,12 @@ if (class_exists('Dotenv\Dotenv') && file_exists(CRAFT_BASE_PATH . '/.env')) {
     Dotenv\Dotenv::create(CRAFT_BASE_PATH)->load();
 }
 
-// Load and run Craft
+// Define additional PHP constants
+// (see https://craftcms.com/docs/3.x/config/#php-constants)
 define('CRAFT_ENVIRONMENT', getenv('ENVIRONMENT') ?: 'production');
+// ...
+
+// Load and run Craft
 /** @var craft\web\Application $app */
 $app = require CRAFT_VENDOR_PATH . '/craftcms/cms/bootstrap/web.php';
 $app->run();
