37d607337f1d9b41ff0c0c6ef13c5202303823f9
Brandon Kelly <brandon@pixelandtonic.com>
2021-12-15 20:19:48 -0800

    Remove `// ...`

--- bootstrap.php
+++ bootstrap.php
@@ -18,4 +18,3 @@ if (class_exists('Dotenv\Dotenv')) {
 // Define additional PHP constants
 // (see https://craftcms.com/docs/3.x/config/#php-constants)
 define('CRAFT_ENVIRONMENT', getenv('ENVIRONMENT') ?: 'production');
-// ...
