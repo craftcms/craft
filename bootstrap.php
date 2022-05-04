b1f176a66eed0fbc946ee6526066f3c4784683b7
Tim Kelty <tim@craftcms.com>
2022-05-03 23:54:56 -0400

    Use ::class instead of string

--- bootstrap.php
+++ bootstrap.php
@@ -11,7 +11,7 @@ define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH . '/vendor');
 require_once CRAFT_VENDOR_PATH . '/autoload.php';
 
 // Load dotenv?
-if (class_exists('Dotenv\Dotenv')) {
+if (class_exists(Dotenv\Dotenv::class)) {
     // By default, this will allow .env file values to override environment variables
     // with matching names. Use `createUnsafeImmutable` to disable this.
     Dotenv\Dotenv::createUnsafeMutable(CRAFT_BASE_PATH)->safeLoad();
