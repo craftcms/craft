60bddeefb040bc878ae9f42fda5231f52ab70b56
smonist <s.wesp@gmx.net>
2023-02-04 14:11:38 +0100

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	host: true,
+	server: {
+		host: '0.0.0.0',
+		port: 3000
+	},
 	plugins: [
 		liveReload(["./templates/**/*"]),
 		legacy({
