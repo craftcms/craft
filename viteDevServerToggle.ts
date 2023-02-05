0e96804ada11e6fb0a26e91f37f68b1ffbd3bc29
smonist <s.wesp@gmx.net>
2023-02-05 22:55:31 +0100

    re-add vite dev server toggle

---
+++ viteDevServerToggle.ts
@@ -0,0 +1,29 @@
+/**
+ * toggle USE_VITE_DEV_SERVER environment variable based on which npm script is running
+ * */
+const fs = require("fs");
+const os = require("os");
+
+
+function setEnvValue(key, value) {
+
+	// read file from hdd & split if from a linebreak to a array
+	const ENV_VARS = fs.readFileSync("./.env", "utf8").split(os.EOL);
+
+	// find the env we want based on the key
+	const target = ENV_VARS.indexOf(ENV_VARS.find((line) => {
+		return line.match(new RegExp(key));
+	}));
+
+	// replace the key/value with the new value
+	ENV_VARS.splice(target, 1, `${key}=${value}`);
+
+	// write everything back to the file system
+	fs.writeFileSync("./.env", ENV_VARS.join(os.EOL));
+
+}
+
+if (process.argv.length > 2) {
+	const enable = process.argv[2];
+	setEnvValue("USE_VITE_DEV_SERVER", enable);
+}
