f378ad840b06216e70f2dac59f1f55dc3e2e3b08
smonist <s.wesp@gmx.net>
2023-02-06 12:35:31 +0100

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,29 @@
 import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
 import liveReload from "vite-plugin-live-reload";
+import critical from "rollup-plugin-critical";
 import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	server: {
-		host: '0.0.0.0',
-		port: 3000
-	},
-	plugins: [
-		liveReload(["./templates/**/*"]),
-		legacy({
-			targets: ["defaults"],
-			additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
-		}),
-		critical({
-			criticalUrl: "http://localhost",
-			criticalBase: "./web/dist/criticalcss/",
-			criticalPages: [{ uri: "/", template: "index" }],
-			criticalConfig: {},
-		}),
-		viteCompression(),
-	],
-});
+export default ({ command }) =>
+	defineConfig({
+		base: command === "serve" ? "" : "/dist/",
+		server: {
+			host: "0.0.0.0",
+			port: 3000,
+		},
+		plugins: [
+			liveReload(["./templates/**/*"]),
+			legacy({
+				targets: ["defaults"],
+				additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+			}),
+			critical({
+				criticalUrl: "http://localhost",
+				criticalBase: "./web/dist/criticalcss/",
+				criticalPages: [{ uri: "/", template: "index" }],
+				criticalConfig: {},
+			}),
+			viteCompression(),
+		],
+	});
