950cd7fd48d071b09b8c375e8b15f4362a1eb951
smonist <s.wesp@gmx.net>
2023-02-07 23:11:17 +0100

    do not install from packagist dev branch anymore

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 ddev config --project-type=craftcms --docroot=web --create-docroot
-ddev composer create -y -s dev smonist/craft-vite-starter
+ddev composer create -y smonist/craft-vite-starter
 make install
 make dev
 ddev launch
