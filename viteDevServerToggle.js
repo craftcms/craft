717fe1270b4bf24e8ded5b7946b079b13a96e5f4
smonist <s.wesp@gmx.net>
2023-08-05 12:40:56 +0200

    revert previous commit
    switching to type: module is not feasible here

--- viteDevServerToggle.js
+++ viteDevServerToggle.js
@@ -1,25 +1,25 @@
 /**
  * toggle USE_VITE_DEV_SERVER environment variable based on which npm script is running
  * */
-import { readFileSync, writeFileSync} from "fs";
-import { EOL} from "os";
+const fs = require("fs");
+const os = require("os");
 
 function setEnvValue(key, value) {
 	// read file from hdd & split if from a linebreak to an array
-	const ENV_VARS = readFileSync("./.env", "utf8").split(EOL);
+	const ENV_VARS = fs.readFileSync("./.env", "utf8").split(os.EOL);
 
 	// find the env we want based on the key
 	const target = ENV_VARS.indexOf(
 		ENV_VARS.find((line) => {
 			return line.match(new RegExp(key));
-		}) ?? ''
+		}) ?? ""
 	);
 
 	// replace the key/value with the new value
 	ENV_VARS.splice(target, 1, `${key}=${value}`);
 
 	// write everything back to the file system
-	writeFileSync("./.env", ENV_VARS.join(EOL));
+	fs.writeFileSync("./.env", ENV_VARS.join(os.EOL));
 }
 
 if (process.argv.length > 2) {
