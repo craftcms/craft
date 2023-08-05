3e9c9e4e0b0c783c5f07edb3e933d944031998a3
smonist <s.wesp@gmx.net>
2023-08-05 12:34:15 +0200

    update viteDevServerToggle to modern JS

--- viteDevServerToggle.js
+++ viteDevServerToggle.js
@@ -1,25 +1,25 @@
 /**
  * toggle USE_VITE_DEV_SERVER environment variable based on which npm script is running
  * */
-const fs = require("fs");
-const os = require("os");
+import { readFileSync, writeFileSync} from "fs";
+import { EOL} from "os";
 
 function setEnvValue(key, value) {
-	// read file from hdd & split if from a linebreak to a array
-	const ENV_VARS = fs.readFileSync("./.env", "utf8").split(os.EOL);
+	// read file from hdd & split if from a linebreak to an array
+	const ENV_VARS = readFileSync("./.env", "utf8").split(EOL);
 
 	// find the env we want based on the key
 	const target = ENV_VARS.indexOf(
 		ENV_VARS.find((line) => {
 			return line.match(new RegExp(key));
-		})
+		}) ?? ''
 	);
 
 	// replace the key/value with the new value
 	ENV_VARS.splice(target, 1, `${key}=${value}`);
 
 	// write everything back to the file system
-	fs.writeFileSync("./.env", ENV_VARS.join(os.EOL));
+	writeFileSync("./.env", ENV_VARS.join(EOL));
 }
 
 if (process.argv.length > 2) {
