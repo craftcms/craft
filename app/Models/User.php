9004c0388bdac52919199224815259a44005d5fa
Rias <hey@rias.be>
2026-06-16 21:17:06 +0200

    User can now extend Craft's user eloquent model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,17 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use CraftCms\Cms\User\Models\User as CraftUser;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends CraftUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
